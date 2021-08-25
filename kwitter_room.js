137e363c2bbd7fbe25eb4bf99aa4a28d4d005d45
cherrysiddharth <83766444+cherrysiddharth@users.noreply.github.com>
2021-08-25 09:27:24 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,13 @@
+
+//ADD YOUR FIREBASE LINKS HERE
+var firebaseConfig = {
+      apiKey: "AIzaSyCHAizjpnzgNrZ2rQZtKOtpRS0h0RS6gaQ",
+      authDomain: "covid19bot-gxul.firebaseapp.com",
+      databaseURL: "https://covid19bot-gxul-default-rtdb.firebaseio.com",
+      projectId: "covid19bot-gxul",
+      storageBucket: "covid19bot-gxul.appspot.com",
+      messagingSenderId: "757999173122",
+      appId: "1:757999173122:web:2906e6b4fe774747e2f11b",
+      measurementId: "G-1SYCXTMC3H"
+    };
+  
